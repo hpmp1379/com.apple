b0b55a902f817bfb88c57345cc333538be24df11
Min Park <88412085+hpmp1379@users.noreply.github.com>
2022-10-20 07:49:58 +0900

    10-20_update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
 $(function(){
-    $('.gnb li').mouseover(function(){
-        $(this).children('.sub').stop().slideDown();
+    // header gnb/sub dropdown
+    // $('.gnb li').mouseover(function(){
+    //     $(this).children('.sub').stop().slideDown();
+    // })
+    // $('.gnb li').mouseleave(function(){
+    //     $(this).children('.sub').stop().slideUp();
+    // })
+
+    // Content 3
+    let $cntBenefitOffset = $('.cntBenefit').offset();
+    $(document).on('scroll',function(){ //scroll 하면 옆에서 슬라이드하는 BG 등장
+        let $windowTop = $(window).scrollTop();
+        $cntBenefitTop = $cntBenefitOffset.top * 0.9; //offsetTop 임의로 정해진 값
+        if( $windowTop >= $cntBenefitTop){
+            $('.slideBG').css({"left":"0"});
+        }else if($windowTop < $cntBenefitTop){ // 슬라이드 BG 사라짐
+            $('.slideBG').css({'left':'100vw'});
+        }
     })
-    $('.gnb li').mouseleave(function(){
-        $(this).children('.sub').stop().slideUp();
-    })
+    // End of Content 3
 })
